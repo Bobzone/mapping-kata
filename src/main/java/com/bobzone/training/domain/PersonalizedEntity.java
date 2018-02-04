87f32da6bb65be8ada0a85fd2599689a0a980230
Piotr Bobinski <piotr.bobinski@ericsson.com>
2018-02-04 18:37:46 +0100

    add UUID to entities, add proper equals and hashcode

--- src/main/java/com/bobzone/training/domain/PersonalizedEntity.java
+++ src/main/java/com/bobzone/training/domain/PersonalizedEntity.java
@@ -5,6 +5,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
+import static java.util.UUID.randomUUID;
+
 @MappedSuperclass
 // Don't use @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS) here.
 // @Inheritance is to be used for modelling the OOP aspects of domain
@@ -13,8 +15,9 @@ public abstract class PersonalizedEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private
-    Long id;
+    private Long id;
+
+    private String UUID = randomUUID().toString();
 
     public Long getId() {
         return id;
@@ -22,5 +25,24 @@ public abstract class PersonalizedEntity {
 
     public void setId(final Long id) {
         this.id = id;
+    }
+
+    public String getUUID() {
+        return UUID;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        final PersonalizedEntity that = (PersonalizedEntity) o;
+
+        return UUID.equals(that.UUID);
+    }
+
+    @Override
+    public int hashCode() {
+        return UUID.hashCode();
     }
 }
